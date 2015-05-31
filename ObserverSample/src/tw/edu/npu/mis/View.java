953b021dd713c3b59bffd9aa3a1ca861cbd2ee89
miss32123 <miss32123@gmail.com>
2015-05-31 21:34:00 +0800

    rewrite View

--- ObserverSample/src/tw/edu/npu/mis/View.java
+++ ObserverSample/src/tw/edu/npu/mis/View.java
@@ -30,7 +30,7 @@ package tw.edu.npu.mis;
  *
  * @author Samael Wang <freesamael@gmail.com>
  */
-public class View {
+public class View implements Observer, Show{
 
     private final String mName;
     private final Window mWindow;
@@ -52,8 +52,16 @@ public class View {
     /**
      * Show the content of the model on the console.
      */
-    public void onDraw() {
-        System.out.println("View (" + mName + "): " + mModel.getData());
+  
+    @Override
+    public void update() {
+        invalidate();
     }
 
+    @Override
+    public void OnDraw() {
+         System.out.println("View (" + mName + "): " + mModel.getData());
+    }
+
+  
 }
